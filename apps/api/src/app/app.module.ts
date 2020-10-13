f905434f97cb0910089dd432729afc90a0fab1d9
Kiwagu <kiwagu@gmail.com>
2020-10-13 18:45:16 +0200

    ADDED: init hero and anime modules

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HeroModule } from './hero/hero.module';
+import { AnimeModule } from './anime/anime.module';
 
 @Module({
-  imports: [],
+  imports: [HeroModule, AnimeModule],
   controllers: [AppController],
   providers: [AppService],
 })
